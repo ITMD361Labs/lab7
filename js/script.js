7149b42af263bffa2a6bb633316cf69f47757f2c
olemeha <77707992+olemeha@users.noreply.github.com>
2021-04-13 21:40:04 -0500

    return code back to what I originally wrote because the problem was with the directory to the js file

--- js/script.js
+++ js/script.js
@@ -1,16 +1,16 @@
 function init(){
 //add your javascrip between these two lines of code
  	
- 	
+	//create variables
+	var inputEntry = document.getElementById('entryinput');
+	var outputText = document.getElementById('textoutput');
+	var inputButton = document.getElementById('entrybutton');
  	//create function to handle the event of the alert
  	function myEventFunction(){
- 		//create variables
-		var inputEntry = document.getElementById('entryinput');
-		var outputText = document.getElementById('textoutput');
  		alert("Oleksandr Lemeha: " + inputEntry.value);
  		outputText.innerHTML = inputEntry.value;
  	}
- 	var inputButton = document.getElementById('entrybutton');
+ 	
 
  	//execute the myEventHandler() function when the button is pressed
  	inputButton.addEventListener('click', myEventFunction);
