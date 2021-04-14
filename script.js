fe14effdea6946fb3cc5c1e04a3c15ef28af496c
olemeha <77707992+olemeha@users.noreply.github.com>
2021-04-13 19:21:27 -0500

    write javascript code in order to produce an alert

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 function init(){
 //add your javascrip between these two lines of code
- 
-
-
-
-
-
-
-
+ 	//create variables
+ 	var inputEntry = document.getElementById("entryinput");
+ 	var inputButton = document.getElementById("entrybutton");
+ 	var outputText = document.getElementById("textoutput");
+ 	
+ 	//create function to handle the event of the alert
+ 	function myEventHandler(){
+ 		if(inputEntry.value == ''){
+ 			alert("No Input, Please Enter Text");
+ 		}
+ 		else{
+ 		 	alert("Oleksandr Lemeha: " + inputEntry.value);
+ 		 	outputText.innerHTML = inputEntry.value; 
+ 		}
+ 	}
+ 	
+ 	//execute the myEventHandler() function when the button is pressed
+ 	inputButton.addEventListener("click", myEventHandler);
+}
 window.addEventListener('load', init);
