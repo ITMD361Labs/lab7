49c6b7c7522a8e06f1843d3a22f49f65cfd01fa0
olemeha <77707992+olemeha@users.noreply.github.com>
2021-04-13 21:24:16 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 function init(){
 //add your javascrip between these two lines of code
- 	//create variables
- 	var inputEntry = document.getElementById('entryinput');
- 	var inputButton = document.getElementById('entrybutton');
- 	var outputText = document.getElementById('textoutput');
+ 	
  	
  	//create function to handle the event of the alert
  	function myEventHandler(){
+ 		//create variables
+		var inputEntry = document.getElementById('entryinput');
+		var outputText = document.getElementById('textoutput');
  		alert("Oleksandr Lemeha: " + inputEntry.value);
  		outputText.innerHTML = inputEntry.value;
  	}
- 	
+ 	var inputButton = document.getElementById('entrybutton');
+
  	//execute the myEventHandler() function when the button is pressed
  	inputButton.addEventListener('click', myEventHandler);
 }
